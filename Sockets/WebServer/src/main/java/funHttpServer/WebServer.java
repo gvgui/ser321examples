f547ddab42d40e12c6ea1df54186d7f494b22c2e
gvgui <gvgui@asu.edu>
2022-10-25 02:54:43 -0400

    more error handling for github request

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -257,6 +257,14 @@ class WebServer {
 
           if (request.indexOf("?") > 1) {
             try {
+              query_pairs = splitQuery(request.replace("github?", ""));
+              String json = fetchURL("https://api.github.com/" + query_pairs.get("query"));
+              //System.out.println(json);
+
+              builder.append("HTTP/1.1 200 OK\n");
+              builder.append("Content-Type: text/html; charset=utf-8\n");
+              builder.append("\n");
+              builder.append("Check the todos mentioned in the Java source file");
 
             }
             catch (Exception e) {
@@ -286,14 +294,6 @@ class WebServer {
               }
             }
           }
-          query_pairs = splitQuery(request.replace("github?", ""));
-          String json = fetchURL("https://api.github.com/" + query_pairs.get("query"));
-          //System.out.println(json);
-
-          builder.append("HTTP/1.1 200 OK\n");
-          builder.append("Content-Type: text/html; charset=utf-8\n");
-          builder.append("\n");
-          builder.append("Check the todos mentioned in the Java source file");
           // TODO: Parse the JSON returned by your fetch and create an appropriate
           // response based on what the assignment document asks for
 
