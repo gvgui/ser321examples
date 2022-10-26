cf50b6394482227f3c355ff2d94635293b785c47
gvgui <gvgui@asu.edu>
2022-10-25 20:23:50 -0400

    new

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -384,7 +384,7 @@ class WebServer {
               builder.append("<html>");
               builder.append("<body>");
               builder.append("The Fruit or Vegetable is: " + head + "<br>");
-              builder.append("<img src=").append('"').append(img).append('"').append(" />");
+              builder.append(@"<img src=").append('"').append(img).append('"').append(" />");
               builder.append("</body>");
               builder.append("</html>");
 
